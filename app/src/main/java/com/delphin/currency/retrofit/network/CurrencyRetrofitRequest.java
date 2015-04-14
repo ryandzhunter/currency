f949f3601658ee596a68483b121b002e9d7accc4
dmitrykudrenko <dmitrykudrenko@8ff631a9-127e-4b00-a855-5f500109e7cb>
2015-04-14 14:07:28 +0000

    refactoring

--- app/src/main/java/com/delphin/currency/retrofit/network/CurrencyRetrofitRequest.java
+++ app/src/main/java/com/delphin/currency/retrofit/network/CurrencyRetrofitRequest.java
@@ -3,77 +3,16 @@ package com.delphin.currency.retrofit.network;
 import com.delphin.currency.api.Api;
 import com.octo.android.robospice.request.retrofit.RetrofitSpiceRequest;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Type;
-import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import greendao.GlobalCourses;
-import retrofit.RestAdapter;
-import retrofit.converter.ConversionException;
-import retrofit.converter.Converter;
-import retrofit.mime.TypedInput;
-import retrofit.mime.TypedOutput;
 
 public class CurrencyRetrofitRequest extends RetrofitSpiceRequest<GlobalCourses, Api> {
-    protected RestAdapter restAdapter;
-    protected Pattern pattern = Pattern.compile(".+\\[(.+)\\,(.+)\\,(.+)\\]");
 
     public CurrencyRetrofitRequest() {
         super(GlobalCourses.class, Api.class);
-
-        restAdapter = new RestAdapter.Builder()
-                .setEndpoint(Api.URL)
-                .setConverter(new ZenRusConverter())
-                .build();
-    }
-
-    private void parseData(TypedInput body, GlobalCourses courses) {
-        try {
-            InputStream in = body.in();
-            java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
-            String response = s.hasNext() ? s.next() : "";
-            Matcher matcher = pattern.matcher(response);
-            if (matcher.matches()) {
-                String usd = matcher.group(1);
-                String eur = matcher.group(2);
-                String oil = matcher.group(3);
-
-                courses.setUsd(Double.parseDouble(usd));
-                courses.setEur(Double.parseDouble(eur));
-                courses.setOil(Double.parseDouble(oil));
-            }
-        } catch (IOException | NumberFormatException ignored) {
-
-        }
     }
 
     @Override
     public GlobalCourses loadDataFromNetwork() throws Exception {
         return getService().getCourses();
-    }
-
-    @Override
-    public Api getService() {
-        return restAdapter.create(Api.class);
-    }
-
-    private class ZenRusConverter implements Converter {
-
-        @Override
-        public Object fromBody(TypedInput body, Type type) throws ConversionException {
-            GlobalCourses courses = new GlobalCourses();
-            courses.setDate(new Date());
-
-            parseData(body, courses);
-            return courses;
-        }
-
-        @Override
-        public TypedOutput toBody(Object object) {
-            return null;
-        }
     }
 }
--- app/src/main/java/com/delphin/currency/retrofit/network/CurrencyRetrofitService.java
+++ app/src/main/java/com/delphin/currency/retrofit/network/CurrencyRetrofitService.java
@@ -5,6 +5,20 @@ import com.octo.android.robospice.retrofit.RetrofitGsonSpiceService;
 
 import org.androidannotations.annotations.EService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import greendao.GlobalCourses;
+import retrofit.RestAdapter;
+import retrofit.converter.ConversionException;
+import retrofit.converter.Converter;
+import retrofit.mime.TypedInput;
+import retrofit.mime.TypedOutput;
+
 @EService
 public class CurrencyRetrofitService extends RetrofitGsonSpiceService {
     @Override
@@ -16,5 +30,50 @@ public class CurrencyRetrofitService extends RetrofitGsonSpiceService {
     public void onCreate() {
         super.onCreate();
         addRetrofitInterface(Api.class);
+    }
+
+    @Override
+    protected RestAdapter.Builder createRestAdapterBuilder() {
+        RestAdapter.Builder builder = super.createRestAdapterBuilder();
+        builder.setConverter(new ZenRusConverter());
+        return builder;
+    }
+
+    private class ZenRusConverter implements Converter {
+        protected Pattern pattern = Pattern.compile(".+\\[(.+)\\,(.+)\\,(.+)\\]");
+
+        @Override
+        public Object fromBody(TypedInput body, Type type) throws ConversionException {
+            GlobalCourses courses = new GlobalCourses();
+            courses.setDate(new Date());
+
+            parseData(body, courses);
+            return courses;
+        }
+
+        @Override
+        public TypedOutput toBody(Object object) {
+            return null;
+        }
+
+        private void parseData(TypedInput body, GlobalCourses courses) {
+            try {
+                InputStream in = body.in();
+                java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+                String response = s.hasNext() ? s.next() : "";
+                Matcher matcher = pattern.matcher(response);
+                if (matcher.matches()) {
+                    String usd = matcher.group(1);
+                    String eur = matcher.group(2);
+                    String oil = matcher.group(3);
+
+                    courses.setUsd(Double.parseDouble(usd));
+                    courses.setEur(Double.parseDouble(eur));
+                    courses.setOil(Double.parseDouble(oil));
+                }
+            } catch (IOException | NumberFormatException ignored) {
+
+            }
+        }
     }
 }
