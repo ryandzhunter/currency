00d5094f942c94e0ff7494d0a17a2aa82d2c44ad
dmitrykudrenko <dmitrykudrenko@8ff631a9-127e-4b00-a855-5f500109e7cb>
2015-04-16 06:50:12 +0000

    ottobus improvement

--- app/src/main/java/com/delphin/currency/otto/OttoBus.java
+++ app/src/main/java/com/delphin/currency/otto/OttoBus.java
@@ -1,5 +1,7 @@
 package com.delphin.currency.otto;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.squareup.otto.BasicBus;
@@ -8,6 +10,8 @@ import org.androidannotations.annotations.EBean;
 
 @EBean(scope = EBean.Scope.Singleton)
 public class OttoBus extends BasicBus {
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+
     @Override
     public void unregister(Object object) {
         try {
@@ -15,5 +19,17 @@ public class OttoBus extends BasicBus {
         } catch (IllegalArgumentException e) {
             Log.e("Object isn't registered", e.getMessage(), e);
         }
+    }
+
+    @Override
+    public void post(final Object event) {
+        if (Looper.myLooper() != Looper.getMainLooper()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    OttoBus.super.post(event);
+                }
+            });
+        } else super.post(event);
     }
 }
